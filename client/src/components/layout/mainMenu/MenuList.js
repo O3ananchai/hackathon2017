64e44f3a8e88bdf99889b5429373e5635f9d899a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-24 17:55:18 +0700

    fix menu

--- client/src/components/layout/mainMenu/MenuList.js
+++ client/src/components/layout/mainMenu/MenuList.js
@@ -6,13 +6,7 @@ class MenuList extends PureComponent {
   render() {
     return (
       <ul className="nav">
-        <MenuItem icon="dashboard" path="/dashboard" />
-        <MenuItem icon="assignment" path="/manage-booking" />
-        <MenuItem icon="chrome_reader_mode" path="/booking-summary" />
-        <MenuItem icon="directions_bus" path="/print-bus-path" />
-        <MenuItem icon="edit_location" path="/manage-bus-path" />
-        <MenuItem icon="widgets" path="/manage-tour-package" />
-        <MenuItem icon="contact_phone" path="/manage-agent" />
+        <MenuItem icon="assignment" path="/search-room" />
       </ul>
     )
   }
