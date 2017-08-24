662d5343dca870ba5fb0c03f4ea5cc3dbb75637e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-24 17:54:05 +0700

    import components

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import registerServiceWorker from './registerServiceWorker'
+import App from './App'
+import store from './store'
+
+import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-select/dist/react-select.css'
+import './resources/css/material-dashboard.css'
+import 'react-bootstrap-table/css/react-bootstrap-table.css'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
