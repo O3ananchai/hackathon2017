e41637fa50bbd70b174bfe369688f1fc2bb80a23
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-24 18:52:34 +0700

    add owner schema

---
+++ server/src/models/owner.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const ownerSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'กรุณาระบุชื่อ']
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, 'กรุณาระบุหมายเลขโทรศัพท์']
+  }
+})
+
+const Owner = mongoose.model('Owner', ownerSchema)
+
+module.exports = Owner
