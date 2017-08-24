5746333131393837c2921f4270a3517b397e7e04
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-24 18:06:15 +0700

    add room datatable component

---
+++ client/src/pages/searchRoom/RoomDataTable.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import { connect } from 'react-redux'
+
+import * as actions from '../../actions'
+
+class RoomDataTable extends PureComponent {
+  componentDidMount() {
+    this.props.fetchRooms()
+  }
+
+  render() {
+    const { rooms } = this.props
+    return (
+      <BootstrapTable
+        data={Object.values(rooms)}
+        height={400}
+        search
+        striped
+        hover
+        condensed
+        options={{ clearSearch: true }}
+      >
+        <TableHeaderColumn hidden dataField="_id" isKey>
+          Room ID
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="owner.name">
+          ชื่อเจ้าของ
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="address">ที่อยู่</TableHeaderColumn>
+        <TableHeaderColumn dataField="telephone">
+          หมายเลขโทรศัพท์
+        </TableHeaderColumn>
+        <TableHeaderColumn
+          headerAlign="right"
+          dataAlign="right"
+          dataSort
+          dataField="price"
+        >
+          ราคา
+        </TableHeaderColumn>
+      </BootstrapTable>
+    )
+  }
+}
+
+const mapStateToProps = ({ room: { rooms } }) => ({ rooms })
+
+export default connect(mapStateToProps, actions)(RoomDataTable)
