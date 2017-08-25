ee3808ca7c098b54fb55201fca7d043b4b1a8e90
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-25 09:33:43 +0700

    fix options value to string

--- client/src/pages/searchRoom/searchOptions.js
+++ client/src/pages/searchRoom/searchOptions.js
@@ -1,22 +1,22 @@
 module.exports = [
   {
-    value: { startPrice: 0, endPrice: 1000 },
+    value: '0#1000',
     label: '0-1,000'
   },
   {
-    value: { startPrice: 1001, endPrice: 3000 },
+    value: '1001#3000',
     label: '1,001-3,000'
   },
   {
-    value: { startPrice: 3001, endPrice: 6000 },
+    value: '3001#6000',
     label: '3,001-6,000'
   },
   {
-    value: { startPrice: 6001, endPrice: 10000 },
+    value: '6001#10000',
     label: '6,001-10,000'
   },
   {
-    value: { startPrice: 10000, endPrice: 0 },
+    value: '10000#0',
     label: 'มากกว่า 10,000'
   }
 ]
