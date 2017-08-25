49159ba02f2a22773b5388d9e1ac855ac3ff1cb4
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-25 10:04:03 +0700

    action can filter

--- client/src/actions/searchRoomActions.js
+++ client/src/actions/searchRoomActions.js
@@ -1,12 +1,17 @@
 import { success, error } from 'react-notification-system-redux'
+import split from 'lodash/split'
 import axios from './axios'
 
 import { FETCH_ROOMS_SUCCESS } from './types'
 
-export const fetchRooms = () => async dispatch => {
+export const fetchRooms = ({ price }) => async dispatch => {
+  const [startPrice, endPrice] = split(price, '#')
   try {
-    const { data } = await axios.get('/rooms')
+    const { data } = await axios.get(
+      `/rooms?startPrice=${startPrice}&endPrice=${endPrice}`
+    )
     dispatch({ type: FETCH_ROOMS_SUCCESS, payload: data })
+    dispatch(success({ title: 'แจ้งเตือน', message: 'ค้นหาข้อมูลเรียบร้อย' }))
   } catch (e) {
     dispatch(error({ title: 'แจ้งเตือน', message: e.message }))
   }
