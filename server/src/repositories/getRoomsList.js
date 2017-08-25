f8092e146f9ecd92154e408629d151f9418f6b9e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-25 10:43:32 +0700

    populate owner

--- server/src/repositories/getRoomsList.js
+++ server/src/repositories/getRoomsList.js
@@ -1,9 +1,10 @@
 const Room = require('../models/room')
+const Owner = require('../models/owner')
 
 module.exports = (startPrice, endPrice) => {
   const find =
     endPrice !== '0'
       ? { price: { $gte: startPrice, $lte: endPrice } }
       : { price: { $gte: startPrice } }
-  return Room.find(find)
+  return Room.find(find).populate('owner')
 }
