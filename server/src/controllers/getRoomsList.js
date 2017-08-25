e0665a16be1866338082dd46f28b33996e1c6239
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-25 10:27:31 +0700

    add feature filter by price

--- server/src/controllers/getRoomsList.js
+++ server/src/controllers/getRoomsList.js
@@ -1,6 +1,7 @@
 const repo = require('../repositories')
 
 module.exports = async (req, res, next) => {
-  const rooms = await repo.getRoomsList()
+  const { startPrice, endPrice } = req.query
+  const rooms = await repo.getRoomsList(startPrice, endPrice)
   return res.send(rooms)
 }
