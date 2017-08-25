d47445795167f1cb36743f0475ce6e6d7e1b526e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-25 15:07:32 +0700

    update seed script

--- server/scripts/seed/roomsData.js
+++ server/scripts/seed/roomsData.js
@@ -9,7 +9,10 @@ _.times(1000, () => {
   rooms.push({
     owner: owners[_.random(owners.length - 1)]._id,
     address: faker.address.country(),
+    width: _.random(3, 5),
+    long: _.random(5, 8),
     price: _.random(500, 12000),
+    pledge: _.random(2, 5),
     status: 0
   })
 })
