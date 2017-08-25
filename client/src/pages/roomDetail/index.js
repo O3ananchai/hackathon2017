91d576463352b26f809b0a3bb697a47d228216c7
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-25 15:40:37 +0700

    add roomDetail component

---
+++ client/src/pages/roomDetail/index.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Button from 'react-bootstrap/lib/Button'
+
+import Card from '../../components/Card'
+import DisplayItem from './DisplayItem'
+import * as actions from '../../actions'
+
+class RoomDetail extends PureComponent {
+  componentDidMount() {
+    this.props.fetchRoom(this.props.match.params.id)
+  }
+
+  render() {
+    const { room } = this.props
+    if (!room) {
+      return null
+    }
+    return (
+      <div className="container-fluid">
+        <Card title="รายละเอียดห้องพัก">
+          <form className="form-horizontal">
+            <DisplayItem title="ชื่อ" text={room.owner.name} />
+            <DisplayItem title="ที่อยู่" text={room.address} />
+            <DisplayItem
+              title="ขนาด"
+              text={`${room.width}x${room.long} ตรม.`}
+            />
+            <DisplayItem title="ราคา" text={room.price} />
+            <DisplayItem title="มัดจำ" text={`${room.pledge} เดือน`} />
+            <DisplayItem title="โทรศัพท์" text={room.owner.phoneNumber} />
+            <div className="form-group">
+              <div className="col-sm-offset-2 col-som-10">
+                <Button bsStyle="primary">จอง</Button>
+                <Link className="btn btn-info" to="/search-room">
+                  กลับ
+                </Link>
+              </div>
+            </div>
+          </form>
+        </Card>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ roomDetail: { room } }) => ({ room })
+
+export default connect(mapStateToProps, actions)(RoomDetail)
