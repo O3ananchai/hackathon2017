6f546256a92e1159aeb6b64014ce71d0610a3f1f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-25 19:12:39 +0700

    add customer schema

---
+++ server/src/models/customer.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const customerSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'กรุณาระบุชื่อ']
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, 'กรุณาระบุหมายเลขโทรศัพท์']
+  }
+})
+
+const Customer = mongoose.model('Customer', customerSchema)
+
+module.exports = Customer
