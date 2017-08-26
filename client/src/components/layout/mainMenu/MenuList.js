467bd7227e463c9d1612496c38e601602a83e6a6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-26 20:17:34 +0700

    update menu

--- client/src/components/layout/mainMenu/MenuList.js
+++ client/src/components/layout/mainMenu/MenuList.js
@@ -6,8 +6,13 @@ class MenuList extends PureComponent {
   render() {
     return (
       <ul className="nav">
-        <MenuItem icon="assignment" path="/search-room" text="ค้นหาห้องพัก" />
-        <MenuItem icon="assignment" path="/upload-slip" text="แจ้งชำระเงิน" />
+        <MenuItem
+          icon="account_circle"
+          path="/signin"
+          text="สมัครสมาชิก/เข้าสู่ระบบ"
+        />
+        <MenuItem icon="search" path="/search-room" text="ค้นหาห้องพัก" />
+        <MenuItem icon="cloud_upload" path="/upload-slip" text="แจ้งชำระเงิน" />
         <MenuItem icon="assignment" path="/report" text="รายงาน" />
       </ul>
     )
