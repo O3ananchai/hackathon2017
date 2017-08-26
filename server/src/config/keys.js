d01af5df431d1bc38d7540d39e952ac2b7a45daa
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-26 11:47:30 +0700

    add facebook client id and secret

--- server/src/config/keys.js
+++ server/src/config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
   googleClientID:
     '359926702983-pirkn1krka74a26oulj1fmlq6pfijmmc.apps.googleusercontent.com',
-  googleClientSecret: 'KgIj1Av9n-CDx7CAO_OIp-gq'
+  googleClientSecret: 'KgIj1Av9n-CDx7CAO_OIp-gq',
+  facebookClientID: '347866805635162',
+  facebookClientSecret: '0a2ec3396630392295233aed8c2267df'
 }
