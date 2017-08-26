1afe655b2abf764b8c4f5589737d6af529a561ab
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-26 20:25:42 +0700

    add report route

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import SearchRoom from './pages/searchRoom'
 import RoomDetail from './pages/roomDetail'
 import UploadSlip from './pages/uploadSlip'
 import SignIn from './pages/signIn'
+import Report from './pages/report'
 import { history } from './store'
 
 class App extends PureComponent {
@@ -25,6 +26,7 @@ class App extends PureComponent {
             <Route exact path="/search-room" component={SearchRoom} />
             <Route path="/search-room/:id" component={RoomDetail} />
             <Route path="/upload-slip" component={UploadSlip} />
+            <Route path="/report" component={Report} />
             <Route component={NoMatch} />
           </Switch>
         </Layout>
