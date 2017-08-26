353a5537b54ec712aa192b2c1225a0bcb29d7098
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-26 15:57:53 +0700

    add cookieKey

--- server/src/config/keys.js
+++ server/src/config/keys.js
@@ -3,5 +3,6 @@ module.exports = {
     '359926702983-pirkn1krka74a26oulj1fmlq6pfijmmc.apps.googleusercontent.com',
   googleClientSecret: 'KgIj1Av9n-CDx7CAO_OIp-gq',
   facebookClientID: '347866805635162',
-  facebookClientSecret: '0a2ec3396630392295233aed8c2267df'
+  facebookClientSecret: '0a2ec3396630392295233aed8c2267df',
+  cookieKey: 'slajfioejsj;lvjsldfljl;ksadfjl'
 }
