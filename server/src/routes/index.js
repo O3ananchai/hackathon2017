541d4e516f16df140e4a9fb7347d645343de75a5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-26 11:51:40 +0700

    setup oauth flow for google

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,28 @@
 const c = require('../controllers')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('../config/keys')
 
 module.exports = app => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken)
+        console.log(refreshToken)
+        console.log(profile)
+      }
+    )
+  )
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  )
+  app.get('/auth/google/callback', passport.authenticate('google'))
   app.get('/rooms', c.getRoomsList)
   app.get('/rooms/:id', c.getRoom)
 }
