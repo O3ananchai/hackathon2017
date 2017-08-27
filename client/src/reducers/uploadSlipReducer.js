cabae2a49c1ccd0095cbb0bcf737f66bcce4e2d6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 14:48:19 +0700

    remove select list when upload success

--- client/src/reducers/uploadSlipReducer.js
+++ client/src/reducers/uploadSlipReducer.js
@@ -1,6 +1,10 @@
 import mapKeys from 'lodash/mapKeys'
+import omit from 'lodash/omit'
 
-import { FETCH_NO_SLIP_BOOKINGS_SUCCESS } from '../actions/types'
+import {
+  FETCH_NO_SLIP_BOOKINGS_SUCCESS,
+  UPLOAD_SLIP_SUCCESS
+} from '../actions/types'
 
 const initialState = {
   noSlipBookings: {}
@@ -10,6 +14,12 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_NO_SLIP_BOOKINGS_SUCCESS:
       return { ...state, noSlipBookings: mapKeys(action.payload, '_id') }
+
+    case UPLOAD_SLIP_SUCCESS:
+      return {
+        ...state,
+        noSlipBookings: omit(state.noSlipBookings, action.payload)
+      }
 
     default:
       return state
