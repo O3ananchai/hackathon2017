0e0f9cf5336624452a4b6474bbe8df6c19efccbd
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 16:44:25 +0700

    initial filter owner component

---
+++ client/src/pages/report/FilterOwner.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import map from 'lodash/map'
+import Select from 'react-select'
+
+import * as actions from '../../actions'
+
+class FilterOwner extends PureComponent {
+  componentDidMount() {
+    this.props.fetchOwners()
+  }
+
+  render() {
+    const { owners, owner } = this.props
+    return <Select options={owners} onChange={value => ''} value={owner} />
+  }
+}
+
+const mapStateToProps = ({
+  report: { owners, visibilityFilter: { owner } }
+}) => ({
+  owners: map(owners, owner => ({ value: owner._id, label: owner.name })),
+  owner
+})
+
+export default connect(mapStateToProps, actions)(FilterOwner)
