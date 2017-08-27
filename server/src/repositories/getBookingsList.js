1fa87b1207c79e94f17b40df117899f095f21f7f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 17:09:11 +0700

    populate customer

--- server/src/repositories/getBookingsList.js
+++ server/src/repositories/getBookingsList.js
@@ -1,3 +1,4 @@
 const Booking = require('mongoose').model('Booking')
 
-module.exports = ownerId => Booking.find({ owner: ownerId })
+module.exports = ownerId =>
+  Booking.find({ owner: ownerId }).populate('customer')
