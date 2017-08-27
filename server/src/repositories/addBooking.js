ab54243a72f5d6bea975aeb2d035d7b6ec1c550a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 11:06:59 +0700

    booking must be flip room status

--- server/src/repositories/addBooking.js
+++ server/src/repositories/addBooking.js
@@ -1,3 +1,10 @@
 const Booking = require('mongoose').model('Booking')
+const Room = require('mongoose').model('Room')
 
-module.exports = bookingProps => Booking.create(bookingProps)
+module.exports = async bookingProps => {
+  const [bookingRes, roomRes] = await Promise.all([
+    Booking.create(bookingProps),
+    Room.update({ _id: bookingProps.room._id }, { status: 1 })
+  ])
+  return { bookingRes, roomRes }
+}
