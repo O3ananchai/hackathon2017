92ab233dad10e61ca3983ee06b442fcdf5ea517d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 17:27:00 +0700

    wireup report component

--- client/src/pages/report/index.js
+++ client/src/pages/report/index.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react'
 
+import FilterOwner from './FilterOwner'
 import Card from '../../components/Card'
+import Table from './Table'
 
 class Report extends PureComponent {
   render() {
@@ -8,7 +10,19 @@ class Report extends PureComponent {
       <div className="container-fluid">
         <Card title="รายงาน">
           <div className="row">
-            <div className="col-md-12">Report</div>
+            <div className="col-md-6">
+              <FilterOwner />
+            </div>
+            <div className="col-md-6 col-sm-6">
+              <button className="btn btn-primary pull-right" onClick={() => ''}>
+                พิมพ์รายงาน
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <Table />
+            </div>
           </div>
         </Card>
       </div>
