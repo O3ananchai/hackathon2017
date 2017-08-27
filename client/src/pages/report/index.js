26efd4b7c8ab1d21a2eed43d613c56130856c7d5
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 19:09:38 +0700

    refactor

--- client/src/pages/report/index.js
+++ client/src/pages/report/index.js
@@ -3,13 +3,9 @@ import React, { PureComponent } from 'react'
 import FilterOwner from './FilterOwner'
 import Card from '../../components/Card'
 import Table from './Table'
-import { openReport } from '../../helpers'
+import PrintButton from './PrintButton'
 
 class Report extends PureComponent {
-  openReport = () => {
-    openReport(`bookings`)
-  }
-
   render() {
     return (
       <div className="container-fluid">
@@ -19,12 +15,7 @@ class Report extends PureComponent {
               <FilterOwner />
             </div>
             <div className="col-md-6 col-sm-6">
-              <button
-                className="btn btn-primary pull-right"
-                onClick={this.openReport}
-              >
-                พิมพ์รายงาน
-              </button>
+              <PrintButton />
             </div>
           </div>
           <div className="row">
