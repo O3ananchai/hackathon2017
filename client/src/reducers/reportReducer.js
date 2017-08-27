6e47d7755508fbfee4b73e542b629e008aeae30c
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 17:05:55 +0700

    handle fetch bookings reducer

--- client/src/reducers/reportReducer.js
+++ client/src/reducers/reportReducer.js
@@ -1,13 +1,22 @@
-import { FETCH_OWNERS_SUCCESS } from '../actions/types'
+import { FETCH_OWNERS_SUCCESS, FETCH_BOOKINGS_SUCCESS } from '../actions/types'
 
 const initialState = {
-  owners: []
+  owners: [],
+  bookings: [],
+  visibilityFilter: { owner: null }
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_OWNERS_SUCCESS:
       return { ...state, owners: action.payload }
+
+    case FETCH_BOOKINGS_SUCCESS:
+      return {
+        ...state,
+        bookings: action.payload.data,
+        visibilityFilter: { owner: action.payload.owner }
+      }
 
     default:
       return state
