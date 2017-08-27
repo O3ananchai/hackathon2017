9e36f921c214df83744d41e2f5f3aae6564b2e5c
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 09:25:47 +0700

    if authen must hide sign-in menu

--- client/src/components/layout/mainMenu/MenuList.js
+++ client/src/components/layout/mainMenu/MenuList.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
 
 import MenuItem from './MenuItem'
 
@@ -6,11 +7,13 @@ class MenuList extends PureComponent {
   render() {
     return (
       <ul className="nav">
-        <MenuItem
-          icon="account_circle"
-          path="/sign-in"
-          text="สมัครสมาชิก/เข้าสู่ระบบ"
-        />
+        {!this.props.auth
+          ? <MenuItem
+              icon="account_circle"
+              path="/sign-in"
+              text="สมัครสมาชิก/เข้าสู่ระบบ"
+            />
+          : null}
         <MenuItem icon="search" path="/search-room" text="ค้นหาห้องพัก" />
         <MenuItem icon="cloud_upload" path="/upload-slip" text="แจ้งชำระเงิน" />
         <MenuItem icon="assignment" path="/report" text="รายงาน" />
@@ -19,4 +22,6 @@ class MenuList extends PureComponent {
   }
 }
 
-export default MenuList
+const mapStateToProps = ({ auth }) => ({ auth })
+
+export default connect(mapStateToProps)(MenuList)
