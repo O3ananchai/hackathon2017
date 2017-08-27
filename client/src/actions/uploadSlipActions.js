2693a0165c02ab6d3bbd8e0af99883014fa83d1f
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 14:45:42 +0700

    show noti and resetform when upload success

--- client/src/actions/uploadSlipActions.js
+++ client/src/actions/uploadSlipActions.js
@@ -1,5 +1,6 @@
-import { error } from 'react-notification-system-redux'
+import { success, error } from 'react-notification-system-redux'
 import axios from 'axios'
+import { reset } from 'redux-form'
 
 import { FETCH_NO_SLIP_BOOKINGS_SUCCESS, UPLOAD_SLIP_SUCCESS } from './types'
 
@@ -16,6 +17,13 @@ export const uploadSlip = values => async dispatch => {
   try {
     await axios.put('/api/bookings', values)
     dispatch({ type: UPLOAD_SLIP_SUCCESS, payload: values.bookingId })
+    dispatch(
+      success({
+        title: 'แจ้งเตือน',
+        message: 'อัพโหลดหลักฐานการโอนเงินเรียบร้อย!'
+      })
+    )
+    dispatch(reset('uploadSlip'))
   } catch (e) {
     dispatch(error({ title: 'แจ้งเตือน', message: e.message }))
   }
