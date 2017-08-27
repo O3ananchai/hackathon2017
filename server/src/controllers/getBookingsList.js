1eede13241f27b586cbf889c6eaae41e0008ae48
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 17:06:58 +0700

    when send owner id  must query for owner

--- server/src/controllers/getBookingsList.js
+++ server/src/controllers/getBookingsList.js
@@ -2,6 +2,12 @@ const repo = require('../repositories')
 
 module.exports = async (req, res, next) => {
   const customerId = req.user._id
-  const bookings = await repo.getNoSlipBookingsList(customerId)
+  const ownerId = req.query.ownerId
+  let bookings
+  if (ownerId) {
+    bookings = await repo.getBookingsList(ownerId)
+  } else {
+    bookings = await repo.getNoSlipBookingsList(customerId)
+  }
   return res.send(bookings)
 }
