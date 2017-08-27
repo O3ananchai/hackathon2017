09b9628d79d77d064d8aece81193b6b3be88e1fb
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 12:04:20 +0700

    add submit method

--- client/src/pages/uploadSlip/index.js
+++ client/src/pages/uploadSlip/index.js
@@ -4,13 +4,17 @@ import Card from '../../components/Card'
 import UploadSlipForm from './UploadSlipForm'
 
 class UploadSlip extends PureComponent {
+  onSubmit = values => {
+    console.log(values)
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <Card title="อัพโหลดหลักฐานการโอนเงิน">
           <div className="row">
             <div className="col-md-6 col-md-offset-3">
-              <UploadSlipForm />
+              <UploadSlipForm onSubmit={this.onSubmit} />
             </div>
           </div>
         </Card>
