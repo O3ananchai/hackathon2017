a44be237d9b907d9af63d9963f97772dfc0d69bd
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 09:05:26 +0700

    add logout button

--- client/src/components/layout/toolbar/index.js
+++ client/src/components/layout/toolbar/index.js
@@ -8,6 +8,15 @@ class Toolbar extends PureComponent {
       <nav className="navbar navbar-transparent navbar-absolute">
         <div className="container-fluid">
           <LeftZone />
+          <div className="collapse navbar-collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <a href="/api/logout">
+                  <i className="material-icons">exit_to_app</i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     )
