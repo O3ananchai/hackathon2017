79559de3ba10f9ef1657de8775ce6642577379e4
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-27 18:58:41 +0700

    and open report handle

--- client/src/pages/report/index.js
+++ client/src/pages/report/index.js
@@ -3,8 +3,13 @@ import React, { PureComponent } from 'react'
 import FilterOwner from './FilterOwner'
 import Card from '../../components/Card'
 import Table from './Table'
+import { openReport } from '../../helpers'
 
 class Report extends PureComponent {
+  openReport = () => {
+    openReport(`bookings`)
+  }
+
   render() {
     return (
       <div className="container-fluid">
@@ -14,7 +19,10 @@ class Report extends PureComponent {
               <FilterOwner />
             </div>
             <div className="col-md-6 col-sm-6">
-              <button className="btn btn-primary pull-right" onClick={() => ''}>
+              <button
+                className="btn btn-primary pull-right"
+                onClick={this.openReport}
+              >
                 พิมพ์รายงาน
               </button>
             </div>
