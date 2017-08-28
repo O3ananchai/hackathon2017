95eb72a14fc8476fbcd8fe8978e390b4472b526b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-28 09:18:17 +0700

    refactor api route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,44 +1,5 @@
-const passport = require('passport')
-const c = require('../controllers')
-const requireAuth = require('../middlewares/requireAuth')
-const bookingsReport = require('../reports/bookings')
+const apiRoute = require('./api_route')
 
 module.exports = app => {
-  app.get(
-    '/auth/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
-  )
-  app.get(
-    '/auth/google/callback',
-    passport.authenticate('google', {
-      successRedirect: '/search-room',
-      failureRedirect: '/sign-in'
-    })
-  )
-  app.get(
-    '/auth/facebook',
-    passport.authenticate('facebook', {
-      scope: ['user_about_me', 'manage_pages', 'email']
-    })
-  )
-  app.get(
-    '/auth/facebook/callback',
-    passport.authenticate('facebook', {
-      successRedirect: '/search-room',
-      failureRedirect: '/sign-in'
-    })
-  )
-  app.get('/api/reports/bookings', bookingsReport)
-  app.get('/api/owners', c.getOwnersList)
-  app.get('/api/rooms', c.getRoomsList)
-  app.get('/api/rooms/:id', c.getRoom)
-  app.get('/api/bookings', c.getBookingsList)
-  app.get('/api/bookings/:id', c.getBooking)
-  app.put('/api/bookings', requireAuth, c.updateBooking)
-  app.post('/api/bookings', requireAuth, c.addBooking)
-  app.get('/api/current-user', (req, res) => res.send(req.user))
-  app.get('/api/sign-out', (req, res) => {
-    req.logout()
-    return res.redirect('/')
-  })
+  app.use('/api', apiRoute)
 }
