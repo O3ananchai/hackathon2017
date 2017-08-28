8632614617797e181091c65d27c37115a6f51035
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-28 11:28:02 +0700

    fix seed data script

--- server/scripts/seed/ownersData.js
+++ server/scripts/seed/ownersData.js
@@ -2,13 +2,15 @@ const Owner = require('mongoose').model('Owner')
 const faker = require('faker')
 const _ = require('lodash')
 
-const owners = []
-
-_.times(2, () => {
-  owners.push({
-    name: faker.name.findName(),
+const owners = [
+  {
+    name: 'บ้านป้าแย้ม',
     phoneNumber: faker.phone.phoneNumberFormat()
-  })
-})
+  },
+  {
+    name: 'บ้าน CDG',
+    phoneNumber: faker.phone.phoneNumberFormat()
+  }
+]
 
 module.exports = _.map(owners, owner => new Owner(owner))
--- server/scripts/seed/roomsData.js
+++ server/scripts/seed/roomsData.js
@@ -5,7 +5,28 @@ const owners = require('./ownersData')
 
 const rooms = []
 
-_.times(1000, () => {
+rooms.push([
+  {
+    owner: owners[_.random(owners.length - 1)]._id,
+    address: '71/1 ซ.สุขสะอาด',
+    width: _.random(3, 5),
+    long: _.random(5, 8),
+    price: 200,
+    pledge: _.random(2, 5),
+    status: 0
+  },
+  {
+    owner: owners[_.random(owners.length - 1)]._id,
+    address: '71/1 ซ.สุขสกปรก',
+    width: _.random(3, 5),
+    long: _.random(5, 8),
+    price: 200,
+    pledge: _.random(2, 5),
+    status: 0
+  }
+])
+
+_.times(100, () => {
   rooms.push({
     owner: owners[_.random(owners.length - 1)]._id,
     address: faker.address.country(),
