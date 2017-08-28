81114093ca4202749a6d25dd5dfb213924d393eb
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-28 11:35:59 +0700

    fix seed

--- server/scripts/seed/roomsData.js
+++ server/scripts/seed/roomsData.js
@@ -10,7 +10,7 @@ rooms.push({
   address: '71/1 ซ.สุขสะอาด',
   width: _.random(3, 5),
   long: _.random(5, 8),
-  price: 200,
+  price: 3000,
   pledge: _.random(2, 5),
   status: 0
 })
@@ -20,12 +20,12 @@ rooms.push({
   address: '71/1 ซ.สุขสกปรก',
   width: _.random(3, 5),
   long: _.random(5, 8),
-  price: 200,
+  price: 2000,
   pledge: _.random(2, 5),
   status: 0
 })
 
-_.times(100, () => {
+_.times(10, () => {
   rooms.push({
     owner: owners[_.random(owners.length - 1)]._id,
     address: faker.address.country(),
