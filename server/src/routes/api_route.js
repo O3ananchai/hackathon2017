7eee8c58d82adcc186052040bd7cd202c139c426
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-29 10:56:59 +0700

    fix route

--- server/src/routes/api_route.js
+++ server/src/routes/api_route.js
@@ -1,50 +1,6 @@
 const router = require('express').Router()
-const passport = require('passport')
-const c = require('../controllers')
-const requireAuth = require('../middlewares/requireAuth')
 const bookingsReport = require('../reports/bookings')
 
-router.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-)
-router.get(
-  '/auth/google/callback',
-  passport.authenticate('google', {
-    successRedirect: '/search-room',
-    failureRedirect: '/sign-in'
-  })
-)
-router.get(
-  '/auth/facebook',
-  passport.authenticate('facebook', {
-    scope: ['user_about_me', 'manage_pages', 'email']
-  })
-)
-router.get(
-  '/auth/facebook/callback',
-  passport.authenticate('facebook', {
-    successRedirect: '/search-room',
-    failureRedirect: '/sign-in'
-  })
-)
-router.get('/current-user', (req, res) => res.send(req.user))
-router.get('/sign-out', (req, res) => {
-  req.logout()
-  return res.redirect('/')
-})
-router.get('/seed', c.seedData)
-
-router.get('/rooms', c.getRoomsList)
-router.get('/rooms/:id', c.getRoom)
-
-router.get('/bookings', c.getBookingsList)
-router.get('/bookings/:id', c.getBooking)
-router.put('/bookings', requireAuth, c.updateBooking)
-router.post('/bookings', requireAuth, c.addBooking)
-
 router.get('/reports/bookings', bookingsReport)
-
-router.get('/owners', c.getOwnersList)
 
 module.exports = router
