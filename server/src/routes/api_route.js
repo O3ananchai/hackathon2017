83dfa77a019512a2778043f5a0e56d6e94465cb2
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-30 12:35:28 +0700

    fix bug

--- server/src/routes/api_route.js
+++ server/src/routes/api_route.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const c = require('../controllers')
 
 router.get('/rooms', c.getRoomsList)
-router.get('/rooms/:id', c.getRoom)
 router.get('/rooms/version', (req, res) => res.send({ version: 'rooms-api 1' }))
+router.get('/rooms/:id', c.getRoom)
 
 module.exports = router
