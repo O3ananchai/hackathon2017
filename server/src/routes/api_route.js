297662603e89b3bbcbfbe72963f81883d8a31878
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-30 11:54:18 +0700

    add version route

--- server/src/routes/api_route.js
+++ server/src/routes/api_route.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const bookingsReport = require('../reports/bookings')
 
 router.get('/reports/bookings', bookingsReport)
+router.get('/reports/version', (req, res) => res.send({ version: '1' }))
 
 module.exports = router
