a7e4e13de857cd6960163a9d0080f3d74b9580dd
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-30 11:43:02 +0700

    add version route

--- server/src/routes/api_route.js
+++ server/src/routes/api_route.js
@@ -6,5 +6,6 @@ router.get('/bookings', c.getBookingsList)
 router.get('/bookings/:id', c.getBooking)
 router.put('/bookings', requireAuth, c.updateBooking)
 router.post('/bookings', requireAuth, c.addBooking)
+router.get('/bookings/version', (req, res) => res.send({ version: '1' }))
 
 module.exports = router
