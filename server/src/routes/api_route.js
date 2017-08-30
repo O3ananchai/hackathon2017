e473b8d67202f78e4e3b16040bb8316cf96c6573
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-30 11:49:49 +0700

    add route version

--- server/src/routes/api_route.js
+++ server/src/routes/api_route.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const c = require('../controllers')
 
 router.get('/owners', c.getOwnersList)
+router.get('/owners/version', (req, res) => res.send({ version: '1' }))
 
 module.exports = router
