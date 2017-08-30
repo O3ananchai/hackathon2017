b12f8c266d45fb081744717883d2820375c69512
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-30 12:36:27 +0700

    fix bug

--- server/src/routes/api_route.js
+++ server/src/routes/api_route.js
@@ -3,9 +3,9 @@ const c = require('../controllers')
 const requireAuth = require('../middlewares/requireAuth')
 
 router.get('/bookings', c.getBookingsList)
+router.get('/bookings/version', (req, res) => res.send({ version: 'booking-api 1' }))
 router.get('/bookings/:id', c.getBooking)
 router.put('/bookings', requireAuth, c.updateBooking)
 router.post('/bookings', requireAuth, c.addBooking)
-router.get('/bookings/version', (req, res) => res.send({ version: '1' }))
 
 module.exports = router
