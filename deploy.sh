acfc9b7fc003a784c7612798fe7ca51da0f193ee
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-30 11:56:10 +0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 date > last-deploy
-docker service update --image paiboon15721/hackathon2017-auth-api hackathon_hackathon2017-auth-api
+docker service update --image paiboon15721/hackathon2017-rooms-api hackathon_hackathon2017-rooms-api
 echo "Deploy successful!"
