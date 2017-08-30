08692cd4efb7527a71a5dc39b29484371eb627c6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-08-30 12:26:09 +0700

    test

--- server/src/routes/api_route.js
+++ server/src/routes/api_route.js
@@ -3,6 +3,6 @@ const c = require('../controllers')
 
 router.get('/rooms', c.getRoomsList)
 router.get('/rooms/:id', c.getRoom)
-router.get('/rooms/version', (req, res) => res.send({ version: '1' }))
+router.get('/rooms/version', (req, res) => res.send({ version: 'rooms-api 1' }))
 
 module.exports = router
