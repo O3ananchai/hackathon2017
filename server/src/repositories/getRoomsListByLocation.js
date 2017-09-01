f47f5e5d5249e455f5038e9617d21c63543d943e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-01 17:09:19 +0700

    fix api

--- server/src/repositories/getRoomsListByLocation.js
+++ server/src/repositories/getRoomsListByLocation.js
@@ -2,7 +2,7 @@ const Room = require('mongoose').model('Room')
 
 module.exports = ({ lng, lat }) => {
   return Room.geoNear(
-    { type: 'Point', coordinates: [lng, lat] },
-    { spherical: true, maxDistance: 100000 }
-  ).populate('owner')
+    { type: 'Point', coordinates: [Number(lng), Number(lat)] },
+    { spherical: true, maxDistance: 2000 }
+  )
 }
