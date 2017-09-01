5bdaca37cd2bf50ebe2faa22c6da1c08f545896e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-01 13:35:30 +0700

    wireup

--- client/src/pages/searchRoomByMap/index.js
+++ client/src/pages/searchRoomByMap/index.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 
 import Card from '../../components/Card'
-import Map from './Map'
+import SearchGoogleMap from './SearchGoogleMap'
 import * as actions from '../../actions'
 
 class SearchRoomByMap extends PureComponent {
@@ -11,7 +11,7 @@ class SearchRoomByMap extends PureComponent {
       <div className="container-fluid">
         <Card title="ค้นหาห้องพักด้วยแผนที่">
           <div className="row">
-            <Map />
+            <SearchGoogleMap />
           </div>
         </Card>
       </div>
