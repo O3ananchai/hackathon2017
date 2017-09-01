793717b23dede14b8c7be7833140ba99b68aa1f9
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-01 16:24:57 +0700

    fix geo schema

--- server/src/models/room.js
+++ server/src/models/room.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
+const PointSchema = new Schema({
+  type: { type: String, default: 'Point' },
+  coordinates: { type: [Number], index: '2dsphere' }
+})
+
 const roomSchema = new Schema({
   owner: {
     type: Schema.Types.ObjectId,
     ref: 'Owner'
   },
   address: String,
-  loc: {
-    type: [Number],
-    index: '2d'
-  },
+  geometry: PointSchema,
   width: { type: Number, default: 0 },
   long: { type: Number, default: 0 },
   price: { type: Number, default: 0 },
