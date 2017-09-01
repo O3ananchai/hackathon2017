91d6e7168e3d8d716adf2bb45d0277975ae029a6
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-01 18:48:27 +0700

    update information

--- client/src/pages/searchRoomByMap/SearchGoogleMap/PopUp.js
+++ client/src/pages/searchRoomByMap/SearchGoogleMap/PopUp.js
@@ -1,13 +1,30 @@
 import React, { PureComponent } from 'react'
 import { InfoWindow } from 'react-google-maps'
+import { Link } from 'react-router-dom'
+
+import { numberWithCommas } from '../../../helpers'
 
 class PopUp extends PureComponent {
   render() {
     const { marker, onMarkerClose } = this.props
+    const { obj } = marker.room
 
     return (
       <InfoWindow onCloseClick={() => onMarkerClose(marker)}>
-        <div>{marker.room.address}</div>
+        <ul>
+          <li>
+            <b>ที่อยู่: </b>
+            <Link to={`/search-room/${obj._id}`}>{obj.address}</Link>
+          </li>
+          <li>
+            <b>ราคา: </b>
+            {numberWithCommas(obj.price)} บาท
+          </li>
+          <li>
+            <b>มัดจำ: </b>
+            {obj.pledge} เดือน
+          </li>
+        </ul>
       </InfoWindow>
     )
   }
