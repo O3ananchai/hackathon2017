61a406ab0effb466c3ce1e320290e896e5725e52
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-01 13:56:30 +0700

    refactor

--- client/src/pages/searchRoomByMap/SearchGoogleMap/SearchMap.js
+++ client/src/pages/searchRoomByMap/SearchGoogleMap/SearchMap.js
@@ -1,27 +1,44 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 import SearchBox from 'react-google-maps/lib/places/SearchBox'
 import INPUT_STYLE from './inputStyle'
 const google = window.google
 
-export default withGoogleMap(props => (
-  <GoogleMap
-    ref={props.onMapMounted}
-    defaultZoom={15}
-    onClick={console.log}
-    center={props.center}
-    onBoundsChanged={props.onBoundsChanged}
-  >
-    <SearchBox
-      ref={props.onSearchBoxMounted}
-      bounds={props.bounds}
-      controlPosition={google.maps.ControlPosition.TOP_LEFT}
-      onPlacesChanged={props.onPlacesChanged}
-      inputPlaceholder="สถานที่ใกล้เคียงห้องพัก"
-      inputStyle={INPUT_STYLE}
-    />
-    {props.markers.map((marker, index) => (
-      <Marker position={marker.position} key={index} />
-    ))}
-  </GoogleMap>
-))
+class SearchMap extends PureComponent {
+  render() {
+    const {
+      onMapMounted,
+      center,
+      onBoundsChanged,
+      onSearchBoxMounted,
+      bounds,
+      onPlacesChanged,
+      onMapClick,
+      markers
+    } = this.props
+
+    return (
+      <GoogleMap
+        ref={onMapMounted}
+        defaultZoom={15}
+        onClick={onMapClick}
+        center={center}
+        onBoundsChanged={onBoundsChanged}
+      >
+        <SearchBox
+          ref={onSearchBoxMounted}
+          bounds={bounds}
+          controlPosition={google.maps.ControlPosition.TOP_LEFT}
+          onPlacesChanged={onPlacesChanged}
+          inputPlaceholder="สถานที่ใกล้เคียงห้องพัก"
+          inputStyle={INPUT_STYLE}
+        />
+        {markers.map((marker, index) => (
+          <Marker position={marker.position} key={index} />
+        ))}
+      </GoogleMap>
+    )
+  }
+}
+
+export default withGoogleMap(SearchMap)
