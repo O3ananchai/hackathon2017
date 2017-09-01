d794bbfce17ae0645de156299f8cc30815a3fdf9
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2017-09-01 16:25:13 +0700

    add geo near query

--- server/src/repositories/getRoomsListByLocation.js
+++ server/src/repositories/getRoomsListByLocation.js
@@ -1,5 +1,8 @@
 const Room = require('mongoose').model('Room')
 
-module.exports = () => {
-  return Room.find().populate('owner')
+module.exports = ({ lng, lat }) => {
+  return Room.geoNear(
+    { type: 'Point', coordinates: [lng, lat] },
+    { spherical: true, maxDistance: 100000 }
+  ).populate('owner')
 }
